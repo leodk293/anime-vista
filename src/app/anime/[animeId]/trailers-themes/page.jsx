58a3ee37bc72bbe094771e9d37495b25db5e6cd5
Aboubacar Traore <aboubatrao04@gmail.com>
2025-10-25 22:49:23 +0100

    commit

--- src/app/anime/[animeId]/trailers-themes/page.jsx
+++ src/app/anime/[animeId]/trailers-themes/page.jsx
@@ -57,91 +57,93 @@ export default function TrailerThemePage({ params }) {
     return <Loader />;
   }
 
-  return (
-    <div className="flex flex-col gap-10 text-white">
-      <section className=" flex flex-col gap-5">
-        <div className=" flex flex-col gap-2">
-          <h1 className="text-xl font-bold">Trailers</h1>
-          <span className=" w-[10%] border border-transparent py-1 rounded-full bg-blue-900" />
-        </div>
+  if (!animeVideos.error && animeId && animeVideos) {
+    return (
+      <div className="flex flex-col gap-10 text-white">
+        <section className=" flex flex-col gap-5">
+          <div className=" flex flex-col gap-2">
+            <h1 className="text-xl font-bold">Trailers</h1>
+            <span className=" w-[10%] border border-transparent py-1 rounded-full bg-blue-900" />
+          </div>
 
-        <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-4">
-          {animeVideos.trailers && animeVideos.trailers.length > 0 ? (
-            animeVideos.trailers.map((element, index) => (
-              <div
-                key={nanoid(10)}
-                className="bg-white/5 p-5 flex flex-col gap-1 rounded-lg"
-              >
-                <iframe
-                  className="w-full rounded-sm h-[10rem]"
-                  src={element.trailer.embed_url.replace(
-                    "autoplay=1",
-                    "autoplay=0"
-                  )}
-                  title={`${element?.title} trailer`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-                <div className=" flex flex-col gap-1">
-                  <p className=" font-medium">{element?.title}</p>
-                  <Link target="_blank" href={element.trailer.url}>
-                    <button className=" border border-white bg-transparent px-4 py-1 rounded-sm cursor-pointer flex flex-row justify-center items-center gap-1 text-white">
-                      <p>Watch on Youtube</p>
-                      <Youtube size={20} color="#ffffff" strokeWidth={1.5} />
-                    </button>
-                  </Link>
+          <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-4">
+            {animeVideos.trailers && animeVideos.trailers.length > 0 ? (
+              animeVideos.trailers.map((element, index) => (
+                <div
+                  key={nanoid(10)}
+                  className="bg-white/5 p-5 flex flex-col gap-1 rounded-lg"
+                >
+                  <iframe
+                    className="w-full rounded-sm h-[10rem]"
+                    src={element.trailer.embed_url.replace(
+                      "autoplay=1",
+                      "autoplay=0"
+                    )}
+                    title={`${element?.title} trailer`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                  <div className=" flex flex-col gap-1">
+                    <p className=" font-medium">{element?.title}</p>
+                    <Link target="_blank" href={element.trailer.url}>
+                      <button className=" border border-white bg-transparent px-4 py-1 rounded-sm cursor-pointer flex flex-row justify-center items-center gap-1 text-white">
+                        <p>Watch on Youtube</p>
+                        <Youtube size={20} color="#ffffff" strokeWidth={1.5} />
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <p className=" text-gray-300 font-medium text-lg">
-              No trailer available
-            </p>
-          )}
-        </div>
-      </section>
+              ))
+            ) : (
+              <p className=" text-gray-300 font-medium text-lg">
+                No trailer available
+              </p>
+            )}
+          </div>
+        </section>
 
-      <section className=" flex flex-col gap-5">
-        <div className=" flex flex-col gap-2">
-          <h1 className="text-xl font-bold">Opening And Endings</h1>
-          <span className=" w-[10%] border border-transparent py-1 rounded-full bg-blue-900" />
-        </div>
+        <section className=" flex flex-col gap-5">
+          <div className=" flex flex-col gap-2">
+            <h1 className="text-xl font-bold">Opening And Endings</h1>
+            <span className=" w-[10%] border border-transparent py-1 rounded-full bg-blue-900" />
+          </div>
 
-        <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-4">
-          {animeVideos.musicVideos && animeVideos.musicVideos.length > 0 ? (
-            animeVideos.musicVideos.map((element, index) => (
-              <div
-                key={nanoid(10)}
-                className="bg-white/5 p-5 flex flex-col gap-1 rounded-lg"
-              >
-                <iframe
-                  className="w-full rounded-sm h-[10rem]"
-                  src={element.video.embed_url.replace(
-                    "autoplay=1",
-                    "autoplay=0"
-                  )}
-                  title={`${element?.title} trailer`}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-                <div className=" flex flex-col gap-1">
-                  <p className=" font-medium">{element?.title}</p>
-                  <Link target="_blank" href={element?.video?.url}>
-                    <button className=" border border-white bg-transparent px-4 py-1 rounded-sm cursor-pointer flex flex-row justify-center items-center gap-1 text-white">
-                      <p>Watch on Youtube</p>
-                      <Youtube size={20} color="#ffffff" strokeWidth={1.5} />
-                    </button>
-                  </Link>
+          <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-3 gap-4">
+            {animeVideos.musicVideos && animeVideos.musicVideos.length > 0 ? (
+              animeVideos.musicVideos.map((element, index) => (
+                <div
+                  key={nanoid(10)}
+                  className="bg-white/5 p-5 flex flex-col gap-1 rounded-lg"
+                >
+                  <iframe
+                    className="w-full rounded-sm h-[10rem]"
+                    src={element.video.embed_url.replace(
+                      "autoplay=1",
+                      "autoplay=0"
+                    )}
+                    title={`${element?.title} trailer`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                  <div className=" flex flex-col gap-1">
+                    <p className=" font-medium">{element?.title}</p>
+                    <Link target="_blank" href={element?.video?.url}>
+                      <button className=" border border-white bg-transparent px-4 py-1 rounded-sm cursor-pointer flex flex-row justify-center items-center gap-1 text-white">
+                        <p>Watch on Youtube</p>
+                        <Youtube size={20} color="#ffffff" strokeWidth={1.5} />
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <p className=" text-gray-300 font-medium text-lg">
-              No opening & ending available
-            </p>
-          )}
-        </div>
-      </section>
-    </div>
-  );
+              ))
+            ) : (
+              <p className=" text-gray-300 font-medium text-lg">
+                No opening & ending available
+              </p>
+            )}
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
