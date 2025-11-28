c16a049713a2256de60e22bc3dd922ba632c2a37
Aboubacar Traore <aboubatrao04@gmail.com>
2025-11-28 11:02:54 +0100

    commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
           className={` flex flex-col ${geistSans.variable} ${geistMono.variable} bg-slate-900 mx-auto antialiased `}
         >
           <Header />
-          <div className=" min-h-screen px-3 md:px-0 ">{children}</div>
+          <div className=" min-h-screen mt-[17rem] px-3 md:px-0 md:mt-[11rem] ">{children}</div>
           <Footer />
 
           <Popover>
